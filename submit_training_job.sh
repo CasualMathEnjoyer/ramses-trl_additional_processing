748dc01cb0d3522a78e446a0423499b35b9912be
CasualMathEnjoyer <morovicsova.katka@gmail.com>
2025-12-27 21:43:09 +0100

    update submit script

--- submit_training_job.sh
+++ submit_training_job.sh
@@ -4,7 +4,7 @@
 #PBS -l walltime=24:00:00
 #PBS -q gpu
 #PBS -j oe
-#PBS -l select=1:mem=48G:ncpus=8:ngpus=1
+#PBS -l select=1:mem=32G:ncpus=8:ngpus=1
 
 cd $PBS_O_WORKDIR
 
@@ -16,10 +16,9 @@ conda activate ramses-trl-GPU
 cd /mnt/lustre/helios-home/morovkat/ramses-trl_additional_processing
 
 python train_model.py \
-    --src-train /mnt/lustre/helios-home/morovkat/hiero-transformer/training_data_ramses/source_egy2tnt_cleaned.txt \
-    --tgt-train /mnt/lustre/helios-home/morovkat/hiero-transformer/training_data_ramses/target_egy2tnt_cleaned.txt \
+    --src-train /mnt/lustre/helios-home/morovkat/hiero-transformer/training_data/source_egy2tnt_cleaned.txt \
+    --tgt-train /mnt/lustre/helios-home/morovkat/hiero-transformer/training_data/target_egy2tnt_cleaned.txt \
     --src-val /mnt/lustre/helios-home/morovkat/hiero-transformer/test_and_validation_data/validation_source_egy2tnt_cleaned.txt \
     --tgt-val /mnt/lustre/helios-home/morovkat/hiero-transformer/test_and_validation_data/validation_target_egy2tnt_cleaned.txt \
-    --output /mnt/lustre/helios-home/morovkat/ramses-trl_additional_processing/network_UNICODE/hiero_transformer.h5
-
+    --output /mnt/lustre/helios-home/morovkat/ramses-trl_additional_processing/network/hiero_transformer.h5
 
